8dc3102594a45b08ed82c0924bdff483b394da79
unknown <kevin.williams49531@gmail.com>
2018-05-10 10:42:15 -0400

    logic for running total, beginning of work

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 "use strict";
+"this is a test"
+
 
 
 // // Counter
@@ -12,6 +14,50 @@
 
 
 $(document).ready(function(){
+  // class Sandwich {
+  //   constructor(name, price) {
+  //     this.name =  name;
+  //     this.price = price;
+  //   }
+  // }
+  // class Potions {
+  //   constructor(name, price) {
+  //     this.name =  name;
+  //     this.price = price;
+  //   }
+  // }
+  // class Sides {
+  //   constructor(name, price) {
+  //     this.name =  name;
+  //     this.price = price;
+  //   }
+  // }
+
+  //fuzzy logic for total
+
+//   let total = 0;
+// let x = 3.49;
+// total += x;
+// x = 5.00;
+// total += x;
+
+const items =  [
+  {
+    Name: "BLT",
+    Price: 4
+  }
+  // {
+  //   Name: "PBJ",
+  //   Price: 5
+  // }
+];
+function display () {
+  items.forEach((index) => {
+
+  })
+}
+
+
   // Functionality for CheckoutPopup
   $("#checkout").click(function(){
       $(".checkoutNone").toggleClass("checkoutPopup");
