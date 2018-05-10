b9d5c0eee9fb3e7289631d74f31f245f3a72f1ab
unknown <kevin.williams49531@gmail.com>
2018-05-09 21:08:41 -0400

    Created HTML Layout, Included some JS.

--- code.js
+++ code.js
@@ -1,10 +1,37 @@
 "use strict";
 
-// Counter
-var itemCount = 0;
-$('.add').click(function (){
-  itemCount ++;
-console.log(itemCount);
-}); 
+
+// // Counter
+// var itemCount = 0;
+// $('.add').click(function (){
+//   itemCount ++;
+// console.log(itemCount);
+// });
+
+// Checkout Button
+
+
+$(document).ready(function(){
+  // Functionality for CheckoutPopup
+  $("#checkout").click(function(){
+      $(".checkoutNone").toggleClass("checkoutPopup");
+  });
+  // Functionality for Pay with Cash
+  $("#btnCashNone").click(function(){
+    $(".cashNone").toggleClass("cashShow");
+  });
+  //Functionality for Pay with CC
+  $("#btnCreditNone").click(function(){
+    $(".creditNone").toggleClass("creditShow");
+  });
+  
+
+});
+
+
+
+
+
+
 
 
